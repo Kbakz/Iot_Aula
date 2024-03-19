6afd4eac0f0e80559a072413215e505bc7aac8e0
Kevin Freire <68775535+Kbakz@users.noreply.github.com>
2024-03-19 14:02:39 -0300

    Limpando codigo

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 bussula = 0
 
-def on_gesture_shake():
+def on_button_pressed_a():
     apontarNorte()
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def apontarNorte():
     global bussula
@@ -14,7 +14,7 @@ def apontarNorte():
     elif bussula > 67.5 and bussula <= 112.5:
         basic.show_arrow(ArrowNames.WEST)
     elif bussula > 112.5 and bussula <= 157.5:
-        basic.show_arrow(ArrowNames.SOUTH_EAST)
+        basic.show_arrow(ArrowNames.SOUTH_WEST)
     elif bussula > 157.5 and bussula <= 202.5:
         basic.show_arrow(ArrowNames.SOUTH)
     elif bussula > 202.5 and bussula <= 247.5:
@@ -23,3 +23,7 @@ def apontarNorte():
         basic.show_arrow(ArrowNames.EAST)
     elif bussula > 292.5 and bussula <= 335.5:
         basic.show_arrow(ArrowNames.NORTH_EAST)
+
+def on_button_pressed_b():
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
